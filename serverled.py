e3bf6811fbfe0edb09516e97fab78626270bcf0a
blackrez <nabil.servais@gmail.com>
2012-10-24 08:14:08 +0200

    Options for serial port

--- serverled.py
+++ serverled.py
@@ -3,20 +3,24 @@ from tornado import httpserver
 from tornado import gen
 import tornado.autoreload
 from tornado import web
-import ujson
+from tornado.options import define, options
 import os
 import serial
+
+define("serial", default="/dev/ttyACM0", help="Serial port")
 
 
 class MainHandler(web.RequestHandler):
     def get(self):
+        print "options"
+        print options.serial
         self.render("index2.html")
 
 
 class ServoHandler(web.RequestHandler):
     def get(self):
         servo = self.get_argument("servo", default=None, strip=False)
-        s = serial.Serial('/dev/ttyACM0')
+        s = serial.Serial(options.serial, 9600)
         s.write(servo)
         self.write('ok')
 
@@ -25,7 +29,7 @@ class LedHandler(web.RequestHandler):
     @web.asynchronous
     def get(self):
         led = self.get_argument("led", default=None, strip=False)
-        s = serial.Serial('/dev/ttyACM0')
+        s = serial.Serial(options.serial, 9600)
         result = s.write(led)
         self._read_serial(result, s)
 
@@ -58,11 +62,12 @@ class Application(web.Application):
                         (r"(/static/)",  tornado.web.StaticFileHandler,
                         dict(path=settings['static_path']))
                        ]
-        tornado.web.Application.__init__(self, handlers, **settings)
+        web.Application.__init__(self, handlers, **settings)
 
 
 def main():
     app = Application()
+    options.parse_command_line()
     app.listen(8888)
     io_loop = ioloop.IOLoop.instance().start()
     tornado.autoreload.start(io_loop)
